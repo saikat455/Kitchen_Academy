a795665c9173a3644d48443ce8edb5fa75a75ab3
Saikat <saikat25112000@gmail.com>
2024-09-13 17:48:11 +0600

    trying to solve image issue

--- components/Home/Hero/HeroImage.tsx
+++ components/Home/Hero/HeroImage.tsx
@@ -4,16 +4,14 @@
 
 import Image from 'next/image';
 import React from 'react';
-import Tilt from "react-parallax-tilt";
+
 
 const HeroImage = () => {
     return (
-        <Tilt>
-            <div data-aos='fade-left' data-aos-delay='1050' className='relative w-full h-full'>
-            <Image src="/images/hero.png" layout='responsive' width={800} height={600} alt='hero' />
+        <div className='relative w-full h-full'>
+    <Image src="/images/hero.png" layout='responsive' width={800} height={600} alt='hero' />
+</div>
 
-            </div>
-        </Tilt>
     );
 };
 
