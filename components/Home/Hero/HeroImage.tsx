8f008ff77a9ee36e68807c3ede500e5a3b39a577
Saikat <saikat25112000@gmail.com>
2024-09-13 17:22:01 +0600

    second

--- components/Home/Hero/HeroImage.tsx
+++ components/Home/Hero/HeroImage.tsx
@@ -1,23 +1,5 @@
 
 
-// "use client";
-
-// import Image from 'next/image';
-// import React from 'react';
-// import Tilt from "react-parallax-tilt";
-
-// const HeroImage = ({ className }) => {
-//     return (
-//         <Tilt className={className}>
-//             <div data-aos='fade-left' data-aos-delay='1050' className='relative'>
-//                 <Image src="/images/Hero.png" layout='responsive' width={800} height={600} alt='Hero' />
-//             </div>
-//         </Tilt>
-//     );
-// };
-
-// export default HeroImage;
-
 "use client";
 
 import Image from 'next/image';
@@ -28,7 +10,8 @@ const HeroImage = () => {
     return (
         <Tilt>
             <div data-aos='fade-left' data-aos-delay='1050' className='relative w-full h-full'>
-                <Image src="/images/Hero.png" layout='responsive' width={800} height={600} alt='Hero' />
+                <Image src="public/images/Hero.png
+" layout='responsive' width={800} height={600} alt='Hero' />
             </div>
         </Tilt>
     );
